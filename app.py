460aeba8d577dfe03525fb70ba85473feaa1a11a
naveen <veeravenkatanaveenburra@gmail.com>
2026-03-03 15:46:47 +0530

    modifed the code

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def main():
         elif choice == "4":
             bank.check_account_balance()
 
-        elif choice == "5":
+        elif choice == "7":
             bank.show_account_transactions()
 
         elif choice == "6":
